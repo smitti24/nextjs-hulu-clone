e10e27ff21194ed9f131e50d92609e5c83df6703
Andre Smith <andre.frank.smith@gmail.com>
2022-07-27 21:58:18 +0200

    Implemented header component

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,25 @@
 import React from "react";
 import Image from "next/image";
 import HeaderItem from "./HeaderItem";
+import {
+  HomeIcon,
+  BadgeCheckIcon,
+  CollectionIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
 
 function Hearder() {
   return (
-    <header className="">
-      <div>
-        <HeaderItem />
+    <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
+      <div className="flex flex-grow justify-evenly max-w-2xl ">
+        <HeaderItem title="HOME" Icon={HomeIcon} />
+        <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
+        <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} />
+        <HeaderItem title="COLLECTIONS" Icon={CollectionIcon} />
+        <HeaderItem title="SEARCH" Icon={SearchIcon} />
+        <HeaderItem title="ACCOUNT" Icon={UserIcon} />
       </div>
       <Image
         className="object-contain"
--- components/HeaderItem.js
+++ components/HeaderItem.js
@@ -1,7 +1,17 @@
 import React from "react";
 
-function HeaderItem(props) {
-  return <div>HeaderItem</div>;
+function HeaderItem({ Icon, title }) {
+  return (
+    <div
+      className="flex flex-col items-center 
+        cursor-pointer w-12 sm:w-20 hover:text-white group"
+    >
+      <Icon className="h-8 mb-1 group-hover:animate-bounce" />
+      <p className="opacity-0 group-hover:opacity-100 tracking-widest">
+        {title}
+      </p>
+    </div>
+  );
 }
 
 export default HeaderItem;
