e8fa831a600948ee162a65c2c264b6d87a333ef2
Andre Smith <andre.frank.smith@gmail.com>
2022-07-29 07:39:42 +0200

    Fixed mobile view

--- components/Results.js
+++ components/Results.js
@@ -4,7 +4,7 @@ import FlipMove from "react-flip-move";
 
 export default function Results({ requests }) {
   return (
-    <FlipMove className="px-5 my-10 grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
+    <FlipMove className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
       {requests.map((result) => (
         <Thumbnail key={result.id} results={result} />
       ))}
